c494f8f2ebc0ace40d00fbdc9f3d16e11e9c8dd8
David <david.ruegg.0504@gmail.com>
2021-04-23 16:21:30 +0200

    1st proximity test (+cleaining)

---
+++ DataAcquisition.h
@@ -0,0 +1,34 @@
+/*
+ * DataAcquisition.h
+ *
+ *  Created on: 23 avr. 2021
+ *      Author: druegg
+ */
+
+#ifndef DATAACQUISITION_H_
+#define DATAACQUISITION_H_
+
+#define WALL_FRONT_B 0x01
+#define WALL_BACK_B 0x02
+#define WALL_RIGHT_B 0x04
+#define WALL_LEFT_B 0x08
+
+#define IR1 0
+#define IR2 1
+#define IR3 2
+#define IR4 3
+#define IR5 4
+#define IR6 5
+#define IR7 6
+#define IR8 7
+#define PROXIMITY_THRESHOLD 200
+
+void scan_maze_cell(uint8_t* maze_cell);
+
+/*
+typedef struct {
+	uint8_t	maze_cell
+} maze_cell_t;
+*/
+
+#endif /* DATAACQUISITION_H_ */
--- main.c
+++ main.c
@@ -1,71 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-
-#include "ch.h"
 #include "hal.h"
+#include "ch.h"
 #include "memory_protection.h"
-#include <usbcfg.h>
+
 #include <main.h>
+#include <DataAcquisition.h>
+#include <sensors/proximity.h>
 #include <motors.h>
-#include <camera/po8030.h>
-#include <chprintf.h>
+#include <leds.h>
 
-#include <pi_regulator.h>
-#include <process_image.h>
+messagebus_t bus;
+MUTEX_DECL(bus_lock);
+CONDVAR_DECL(bus_condvar);
 
-void SendUint8ToComputer(uint8_t* data, uint16_t size) 
-{
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)"START", 5);
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)&size, sizeof(uint16_t));
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)data, size);
-}
+int main(void){
+	/*** INITIALIZATION ***/
+	// init ChibiOS + mcu
+	halInit();
+	chSysInit();
+	mpu_init();
 
-static void serial_start(void)
-{
-	static SerialConfig ser_cfg = {
-	    115200,
-	    0,
-	    0,
-	    0,
-	};
+	// init general bus message
+	messagebus_init(&bus, &bus_lock, &bus_condvar);
 
-	sdStart(&SD3, &ser_cfg); // UART3.
-}
-
-int main(void)
-{
-
-    halInit();
-    chSysInit();
-    mpu_init();
-
-    //starts the serial communication
-    serial_start();
-    //start the USB communication
-    usb_start();
-    //starts the camera
-    dcmi_start();
-	po8030_start();
-	//inits the motors
+	// init proximity sensors
+	proximity_start();
+	//init the motors
 	motors_init();
 
-	//stars the threads for the pi regulator and the processing of the image
-	pi_regulator_start();
-	process_image_start();
+	/*** INTERNAL VARIABLES ***/
+	uint8_t actual_cell = 0;
 
-    /* Infinite loop. */
-    while (1) {
-    	//waits 1 second
-        chThdSleepMilliseconds(1000);
-    }
+	/* Infinite loop. */
+	while (1) {
+
+		set_led(NUM_LED,0);
+		scan_maze_cell(&actual_cell);
+
+		//waits 100 us
+		chThdSleepMilliseconds(100);
+	}
 }
 
 #define STACK_CHK_GUARD 0xe2dee396
 uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
 
-void __stack_chk_fail(void)
-{
-    chSysHalt("Stack smashing detected");
+void __stack_chk_fail(void){
+	chSysHalt("Stack smashing detected");
 }
